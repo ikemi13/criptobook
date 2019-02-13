07963818720304d1117bdfa6068d3e3db6800ffa
ikemi13 <fuertesdev@gmail.com>
2019-02-13 19:34:44 +0100

    Extructura de la web

--- app/views.py
+++ app/views.py
@@ -1,6 +1,12 @@
+from flask import render_template
 from app import app
 
 
 @app.route('/')
 def index():
-    return 'Portada web'
+    return render_template('index.html')
+
+
+@app.route('/nuevacompra', methods=['GET', 'POST'])
+def nuevacompra():
+    return render_template('nuevacompra.html')
